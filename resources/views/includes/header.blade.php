f2da57d76ac2d80a930e29bcb39a6833b0e3bdba
Rizal Rizky <mrizalrn@gmail.com>
2023-09-21 06:38:25 +0700

    downgrade laravel

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,79 @@
+<nav
+        class="navbar navbar-expand-lg py-5"
+        style="background-color:#CCE1E2"
+      >
+        <div class="container">
+          <a
+            class="navbar-brand fw-bold"
+            href="/"
+            style="font-size: 28px, color: #01676C"
+          >
+            TemuInklusi
+          </a>
+
+          {{-- Mobile desktop sandwich menu --}}
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 list-inline mx-auto">
+              <li class="list-inline-item me-5">
+                <a
+                  class="nav-link fw-bold"
+                  aria-current="page"
+                  href="#"
+                  style="color: #01676C"
+                >
+                  Home
+                </a>
+              </li>
+              <li class="list-inline-item me-5">
+                <a
+                  class="nav-link fw-bold"
+                  href="#"
+                  style="color: #01676C"
+                >
+                  Event
+                </a>
+              </li>
+              <li class="list-inline-item me-5">
+                <a
+                  class="nav-link fw-bold"
+                  href="#"
+                  style="color: #01676C"
+                >
+                  Blog
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a
+                  class="nav-link fw-bold"
+                  href="#"
+                  style="color:#01676C"
+                >
+                  About
+                </a>
+              </li>
+            </ul>
+
+            <span>
+              <a
+                class="btn rounded-pill px-5 text-white"
+                style="background-color: #01676C"
+                href="#"
+                role="button"
+              >
+                Login
+              </a>
+            </span>
+          </div>
+        </div>
+      </nav>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+<div style="background-color: #CCE1E2">
+    <div class="container text-center justify-content-center pt-3 pb-5">
+      <h3 style="color: #01676C">
+        Cari event yang ramah untuk <strong>Disabilitas?</strong> Temukan
+        hanya di <strong>TemuInklusi</strong>
+      </h3>
+      <div class="my-5">
+        <img src="/public/assets/img/hero.svg" style="max-width: 100%" />
+      </div>
+    </div>
+  </div>
+@endsection
