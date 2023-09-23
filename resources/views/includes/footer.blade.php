e5657f6b969ee4715bdd4b2e37536fb24f044d13
Rizal Rizky <mrizalrn@gmail.com>
2023-09-23 11:56:47 +0700

    add event card and benefit card

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,13 @@
 <footer>
-
+    <div class="container d-flex flex-row justify-content-between">
+        <div class="p-3">
+            <h5 style="font-weight: bold; color: #01676C">TemuInklusi</h5>
+            <p>Satukan Perbedaan, Wujudkan Inklusi<br/>Bersama TemuInklusi!</p>
+        </div>
+        <div class="d-flex flex-column justify-content-end p-3">
+          <p style="font-weight: bold; font-size: 12px">
+            Copyright &copy; 2023 TemuInklusi. All Rights Reserved.
+          </p>
+        </div>
+    </div>
 </footer>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -10,8 +10,30 @@
         hanya di <strong>TemuInklusi</strong>
       </h3>
       <div class="my-5">
-        <img src="/public/assets/img/hero.svg" style="max-width: 100%" />
+        <img src="{{ asset('assets/img/hero.svg')}}" style="max-width: 100%" />
       </div>
     </div>
   </div>
+
+{{-- Benefit Card --}}
+<div class="card m-5" style="width: 18rem;">
+    <div class="d-flex justify-content-center align-items-center py-4">
+        <img src="{{ asset('assets/img/benefitimg.svg')}}" class="card-img-top" alt="event banner" style="max-width: 10rem;" >
+    </div>
+    <div class="card-body text-center px-5">
+      <h5 class="card-title" style="color: #01676C">Aksesibilitas</h5>
+      <p style="color: black; font-size: 16px">Discover sustainable options effortlessly. Our platform offers convenient access to sustainable options all in one place.</p>
+    </div>
+  </div>
+
+{{-- Event Card --}}
+<a href="#" style="text-decoration: none;">
+    <div class="card m-5" style="width: 18rem;">
+      <img src="{{ asset('assets/img/cardimg.png')}}" class="card-img-top" alt="event banner">
+      <div class="card-body">
+        <h5 class="card-title" style="color: #01676C">Power of Words</h5>
+        <p style="color: black; font-size: 12px">8 Agustus 2024</p>
+      </div>
+    </div>
+  </a>
 @endsection
