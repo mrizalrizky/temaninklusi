bbc3e485d765d2e4151f9a2b052712f3d019f8f6
Rizal Rizky <mrizalrn@gmail.com>
2023-09-24 11:41:04 +0700

    homepage done. butuh adjust dikit lagi aja

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div style="background-color: #CCE1E2">
-    <div class="container text-center justify-content-center pt-3 pb-5">
+    <div class="container text-center pt-3 pb-5">
       <h3 style="color: #01676C">
         Cari event yang ramah untuk <strong>Disabilitas?</strong> Temukan
         hanya di <strong>TemuInklusi</strong>
@@ -15,25 +15,60 @@
     </div>
   </div>
 
-{{-- Benefit Card --}}
-<div class="card m-5" style="width: 18rem;">
-    <div class="d-flex justify-content-center align-items-center py-4">
+<section class="container py-5">
+  <h3 class="text-center" style="color: #01676C">
+   Kenapa TemuInklusi?
+  </h3>
+
+  {{-- Benefit Card --}}
+  <div class="d-flex pt-5">
+    <div class="card" style="width: 18rem;">
+        <div class="d-flex justify-content-center align-items-center py-4">
         <img src="{{ asset('assets/img/benefitimg.svg')}}" class="card-img-top" alt="event banner" style="max-width: 10rem;" >
+        </div>
+        <div class="card-body text-center px-5">
+        <h5 class="card-title" style="color: #01676C">Aksesibilitas</h5>
+        <p style="color: black; font-size: 16px">Discover sustainable options effortlessly. Our platform offers convenient access to sustainable options all in one place.</p>
+        </div>
     </div>
-    <div class="card-body text-center px-5">
-      <h5 class="card-title" style="color: #01676C">Aksesibilitas</h5>
-      <p style="color: black; font-size: 16px">Discover sustainable options effortlessly. Our platform offers convenient access to sustainable options all in one place.</p>
+    <div class="card" style="width: 18rem;">
+        <div class="d-flex justify-content-center align-items-center py-4">
+        <img src="{{ asset('assets/img/benefitimg.svg')}}" class="card-img-top" alt="event banner" style="max-width: 10rem;" >
+        </div>
+        <div class="card-body text-center px-5">
+        <h5 class="card-title" style="color: #01676C">Aksesibilitas</h5>
+        <p style="color: black; font-size: 16px">Discover sustainable options effortlessly. Our platform offers convenient access to sustainable options all in one place.</p>
+        </div>
     </div>
   </div>
+</section>
 
-{{-- Event Card --}}
-<a href="#" style="text-decoration: none;">
-    <div class="card m-5" style="width: 18rem;">
-      <img src="{{ asset('assets/img/cardimg.png')}}" class="card-img-top" alt="event banner">
-      <div class="card-body">
-        <h5 class="card-title" style="color: #01676C">Power of Words</h5>
-        <p style="color: black; font-size: 12px">8 Agustus 2024</p>
-      </div>
+<section class="container py-5">
+  <h3 class="text-center" style="color: #01676C">
+    Event Pilihan
+  </h3>
+
+  {{-- Event Card --}}
+  <div class="d-flex pt-5">
+    <div class="card" style="width: 18rem;">
+        <img src="{{ asset('assets/img/cardimg.png')}}" class="card-img-top" alt="event banner">
+        <div class="card-body">
+            <h5 class="card-title" style="color: #01676C">Power of Words</h5>
+            <p style="color: black; font-size: 12px">8 Agustus 2024</p>
+        </div>
     </div>
-  </a>
+
+    <div class="card " style="width: 18rem;">
+        <img src="{{ asset('assets/img/cardimg.png')}}" class="card-img-top" alt="event banner">
+        <div class="card-body">
+            <h5 class="card-title" style="color: #01676C">Power of Words</h5>
+            <p style="color: black; font-size: 12px">8 Agustus 2024</p>
+        </div>
+    </div>
+  </div>
+</section>
+
+<section class="container d-flex justify-content-center">
+    <img src="{{ asset('assets/img/ctabanner1.png') }}" style="width: 100%;"/>
+</section>
 @endsection
