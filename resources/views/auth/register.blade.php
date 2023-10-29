613c337eec9c90f3ee665c29732a952ffeadd3d8
Rizal Rizky <mrizalrn@gmail.com>
2023-10-29 23:44:19 +0700

    add cpanel.yml

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,6 +12,17 @@
                         class="border rounded-4 p-4 p-x-custom-2">
                         @csrf
                         <div class="form-group mb-4">
+                            <label for="" class="text-primary fw-bold mb-2">Bagaimana Anda mengidentifikasi diri Anda?</label>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input py-2" type="radio" name="user_type" id="regular_user" checked/>
+                                <label for="regular_user" class="form-check-label text-primary fw-bold mb-2">Pengguna Reguler</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input py-2" type="radio" name="user_type" id="event_organizer"/>
+                                <label for="event_organizer" class="form-check-label text-primary fw-bold mb-2">Event Organizer</label>
+                            </div>
+                        </div>
+                        <div class="form-group mb-4">
                             <label for="exampleInputNama1" class="text-primary fw-bold mb-2">Name</label>
                             <input type="text" class="form-control py-2 @error('name') is-invalid @enderror"
                                 id="exampleInputNama1" aria-describedby="usernameHelp" name="name"
@@ -58,7 +69,7 @@
                                 </div>
                             @enderror
                         </div>
-                        <div class="form-group mb-2">
+                        <div class="form-group mb-4">
                             <label for="exampleInputConfirmPassword1" class="text-primary fw-bold mb-2 @error('password_confirmation') is-invalid @enderror">Confirmation
                                 Password</label>
                             <input type="password"
@@ -72,17 +83,6 @@
                                 @endif
                         </div>
 
-                        <div class="mb-2">
-                            <label for="" class="text-primary fw-bold mb-2">Bagaimana Anda mengidentifikasi diri Anda?</label>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input py-2" type="radio" name="user_type" id="regular_user" checked/>
-                                <label for="regular_user" class="form-check-label text-primary fw-bold mb-2">Pengguna Reguler</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input py-2" type="radio" name="user_type" id="event_organizer"/>
-                                <label for="event_organizer" class="form-check-label text-primary fw-bold mb-2">Event Organizer</label>
-                            </div>
-                        </div>
                         <div class="px-4">
                             <button type="submit" class="btn btn-primary w-100 rounded-4">Daftar</button>
                         </div>
