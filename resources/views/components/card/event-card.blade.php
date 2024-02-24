15ece9b2d95a4f10450fdcc1378c9d43f6e3dccd
Rizal Rizky <mrizalrn@gmail.com>
2024-02-24 10:54:26 +0700

    breakpoint for prod debug

--- resources/views/components/card/event-card.blade.php
+++ resources/views/components/card/event-card.blade.php
@@ -4,6 +4,7 @@
     @endif
     <a href="{{ route('event.index') }}/{{ $event->eventDetail->slug }}">
         <div class="card">
+            @dd(Storage::disk('public'), Storage::disk('public')->url($event->eventBanner->file_path . $event->eventBanner->file_name))
             <img src="{{ Storage::disk('public')->exists($event->eventBanner->file_path) ? Storage::disk('public')->url($event->eventBanner->file_path . $event->eventBanner->file_name) : asset('assets/img/temuinklusi-asset.png') }}" class="card-img-top img-fluid object-fit-cover" style="height: 12rem" alt="Event Banner">
             <div class="card-body d-grid gap-2">
                 <h5 class="mb-0 card-title text-primary elipsis-title">{{ $event->eventDetail->title }}</h5>
