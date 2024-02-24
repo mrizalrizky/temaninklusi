3b280618029caad1af2c64aa4190822755d3f526
Rizal Rizky <mrizalrn@gmail.com>
2024-02-24 11:26:50 +0700

    another breakpoint

--- resources/views/components/card/event-card.blade.php
+++ resources/views/components/card/event-card.blade.php
@@ -5,6 +5,7 @@
     <a href="{{ route('event.index') }}/{{ $event->eventDetail->slug }}">
         <div class="card">
             {{ Storage::disk('public')->url($event->eventBanner->file_path . $event->eventBanner->file_name) }}
+            {{ Storage::disk('public')->exists($event->eventBanner->file_path) ? 'true' : 'false'}}
             <img src="{{ Storage::disk('public')->exists($event->eventBanner->file_path) ? Storage::disk('public')->url($event->eventBanner->file_path . $event->eventBanner->file_name) : asset('assets/img/temuinklusi-asset.png') }}" class="card-img-top img-fluid object-fit-cover" style="height: 12rem" alt="Event Banner">
             <div class="card-body d-grid gap-2">
                 <h5 class="mb-0 card-title text-primary elipsis-title">{{ $event->eventDetail->title }}</h5>
